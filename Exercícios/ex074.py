738d105d86622f0b30a8656abb825bb419b452bc
Eduardo Pessanha <poppessanha@gmail.com>
2020-09-28 14:23:23 -0300

    Atualizado o ex074.py e ex075.py

--- Exercícios/ex074.py
+++ Exercícios/ex074.py
@@ -5,8 +5,18 @@
 # e também indique o menor e o maior valor que estão na tupla.      #
 # ***************************************************************** #
 from random import randint
-
-titulo = '\033[1;33m  Maior e menor valores em Tupla \033[m'
-t = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
-print(t)
-# print(type(t))
+titulo = ' \033[1;36mMaior e menor valores em Tupla\033[m '
+print(f"\n{titulo:*^60}\n")
+t = (randint(0, 10), randint(0, 10), randint(
+    0, 10), randint(0, 10), randint(0, 10))
+for cont in range(0, len(t)):
+    n = t[cont]
+    if cont == 0:
+        maior = menor = t[cont]
+    if n > maior:
+        maior = t[cont]
+    if n < menor:
+        menor = t[cont]
+print(f'Foram gerados os seguintes números: {t}')
+print(f'\nO maior valor foi {maior}')
+print(f'O menor valor foi {menor}\n')
--- Exercícios/ex075.py
+++ Exercícios/ex075.py
@@ -6,9 +6,10 @@
 # B) Em que posição foi digitado o primeiro valor 3.                #
 # C) Quais foram os números pares.                                  #
 # ***************************************************************** #
-
+titulo = ' \033[1;34mAnálise de dados em uma Tupla\033[m '
+print(f'\n{titulo:=^60}\n')
 for cont in range(0, 4):
-    n = int(input('Digit um número: '))
+    n = int(input(' Digite um número: '))
     if cont == 0:
         t1 = n
     elif cont == 1:
@@ -18,6 +19,20 @@ for cont in range(0, 4):
     else:
         t4 = n
     cont += 1
-
 t = (t1, t2, t3, t4)
-print(f'{type(t)} => {t}')
+print('*'*60)
+n1 = t.count(9)
+if n1 == 0:
+    n1 = ' Não foi digitado o valor 9'
+else:
+    n1 = f' O valor 9 foi digitado {t.count(9)} vez(ez)'
+n2 = t.count(3)
+if n2 != 0:
+    n2 = f'O primeiro valor 3 foi digitado na posição {t.index(3)+1}'
+else:
+    n2 = 'Não foi digitado o valor 3'
+cont = 0
+for n in range(0, len(t)):
+    if t[n] % 2 == 0:
+        cont += 1
+print(f'{n1}\n {n2}\n Foram digitados {cont} número(s) pares\n')
