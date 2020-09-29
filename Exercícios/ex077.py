41e3e469e358be80dbeb3056a2ef075f0f5ed654
Eduardo Pessanha <poppessanha@gmail.com>
2020-09-29 19:59:58 -0300

    atualização e revisão

--- Exercícios/ex077.py
+++ Exercícios/ex077.py
@@ -7,14 +7,12 @@
 titulo = ' \033[1;36mContando vogais em Tupla\033[m '
 linha = '*-' * 25
 print(f'\n{titulo:+^60}')
-print(f'\n{linha}')
 palavra = ('carro', 'aviao', 'churrasco', 'teclado', 'computador',
            'aprender', 'programar', 'linguagem', 'Python', 'curso',
            'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'futuro')
 for nome in palavra:
-    print(f'Na palavra {nome.upper()} temos as vogais:', end=' ')
+    print(f'\nNa palavra {nome.upper()} temos as vogais:', end=' ')
     for vogal in nome:
-        if vogal in 'aeiou':
+        if vogal.lower() in 'aeiou':
             print(vogal, end=' ')
-    print()
-print(f'{linha}\n')
+print(f'\n{linha}\n')
