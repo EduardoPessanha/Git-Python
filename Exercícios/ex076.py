8fbc1a0ff440f1b6d46885e6e1aaadb1950c63c6
Eduardo Pessanha <poppessanha@gmail.com>
2020-09-29 09:57:37 -0300

    Atualização ex076.py

--- Exercícios/ex076.py
+++ Exercícios/ex076.py
@@ -5,13 +5,23 @@
 # listagem de preços, organizando os dados em forma tabular.        #
 # ***************************************************************** #
 titulo = ' \033[1;35mLista de Preços com Tupla\033[m '
-print(f'\n{titulo:$^60}\n')
-# Lápis 1.75
-# Borracha 2.50
-# Caderno 12.90
-# Estojo 25.00
-# Transferidor 4.20
-# Compasso 9.99
-# Mochila 120.32
-# Canetas 27.00
-# Livro 34.90
+linha = '=-'*25
+print()
+print(linha)
+print(f'{titulo:$^60}')
+print(linha)
+produto = ('Lapis', 1.75, 'Borracha', 2.50, 'Caderno', 12.90,
+           'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99,
+           'Mochila', 120.32, 'Canetas', 27.00, 'Livro', 34.90)
+for n in range(0, len(produto), 2):
+    print(f'{produto[n]:.<40}R$ {produto[n+1]:>7.2f}')
+print(linha, '\n')
+
+#             Solução do Gustavo Guanabara:
+
+for i in range(0, len(produto)):
+    if i % 2 == 0:
+        print(f'{produto[i]:.<40}', end='')
+    else:
+        print(f'R${produto[i]:>7.2f}')
+print(linha, '\n')
