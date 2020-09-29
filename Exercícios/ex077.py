2ab48bfdef112c96c33f22a24b84d97ba7e79d3e
Eduardo Pessanha <poppessanha@gmail.com>
2020-09-29 19:26:06 -0300

    Update ex077.py

--- Exercícios/ex077.py
+++ Exercícios/ex077.py
@@ -1,10 +1,20 @@
 # ************************** Desafio 077 ************************** #
 #                     Contando vogais em Tupla                      #
-# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela # 
-# do Campeonato Brasileiro de Futebol, na ordem de colocação.       #
-# Depois mostre:                                                    #
-# a) Os 5 primeiros times.                                          #
-# b) Os últimos 4 colocados.                                        #
-# c) Times em ordem alfabética.                                     #
-# d) Em que posição está o time da Chapecoense.                     #
+# Crie um programa que tenha uma tupla com várias palavras (não     #
+# usar acentos). Depois disso, você deve mostrar, para cada palavra,#
+# quais são as suas vogais.                                         #
 # ***************************************************************** #
+titulo = ' \033[1;36mContando vogais em Tupla\033[m '
+linha = '*-' * 25
+print(f'\n{titulo:+^60}')
+print(f'\n{linha}')
+palavra = ('carro', 'aviao', 'churrasco', 'teclado', 'computador',
+           'aprender', 'programar', 'linguagem', 'Python', 'curso',
+           'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'futuro')
+for nome in palavra:
+    print(f'Na palavra {nome.upper()} temos as vogais:', end=' ')
+    for vogal in nome:
+        if vogal in 'aeiou':
+            print(vogal, end=' ')
+    print()
+print(f'{linha}\n')
