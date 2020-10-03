eb4f206583876170aaef846a9bd7e5e01c81bdad
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-03 11:34:27 -0300

    Atualização

--- Exercícios/ex079.py
+++ Exercícios/ex079.py
@@ -1,7 +1,33 @@
 # ************************** Desafio 079 ************************** #
 #                    Valores únicos em uma Lista                    #
-# Crie um programa onde o usuário possa digitar vários valores      # 
+# Crie um programa onde o usuário possa digitar vários valores      #
 # numéricos e cadastre-os em uma lista. Caso o número já exista lá  #
 # dentro, ele não será adicionado. No final, serão exibidos todos   #
 # os valores únicos digitados, em ordem crescente.                  #
 # ***************************************************************** #
+linha = '-='*25
+titulo = ' \033[1;33mValores únicos em uma Lista\033[m '
+print(f'\n{titulo:*^60}\n')
+print(linha)
+# ***************************************************************** #
+listaValores = list()
+continua = ' '
+while True:
+    num = int(input('Digite um número inteiro: '))
+    cont = listaValores.count(num)
+    if cont != 0:
+        print(
+            f'O número {num} já foi adicionado! Não será adicionado novamente.')
+    else:
+        listaValores.append(num)
+    while continua not in 'SN':
+        continua = str(input('Deseja continuar [S/N]? ')).upper().strip()[0]
+        if continua not in 'SN':
+            print('Entrada incorreta! ', end='')
+    if continua in 'N':
+        break
+    else:
+        continua = ' '
+print(linha)
+listaValores.sort()
+print(f'Os valores digitados foram: {listaValores}')
--- Exercícios/ex080.py
+++ Exercícios/ex080.py
@@ -5,3 +5,13 @@
 # inserção (sem usar o sort()). No final, mostre a lista ordenada   #
 # na tela.                                                          # 
 # ***************************************************************** #
+linha = '-='*25
+titulo = ' \033[1;34mLista ordenada sem repetições\033[m '
+print(f'\n{titulo:*^60}\n')
+print(linha)
+# ***************************************************************** #
+lista=list()
+for cont in range(0,5):
+    valor = int(input('Digite um valor: '))
+    lista.append(valor)
+print(lista)
