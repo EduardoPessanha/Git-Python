883dc41c19242df2edb89defee1d9f9768e3a8d6
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-05 16:07:50 -0300

    Atualização do arquivo ex080.py

--- Exercícios/ex080.py
+++ Exercícios/ex080.py
@@ -16,60 +16,65 @@ for cont in range(0, 5):
     if cont == 0:
         lista.insert(0, valor)
         print(f'O valor {valor}, foi adiconado no final da lista!')
-        # indice = lista.index(valor)
-        # print('O tamanho da lista é ', len(lista))
-    elif cont == 1:                     #valor not in lista:
-        if valor > max(lista):
-            lista.insert(len(lista), valor)
-            indice = list.index(valor)
-            print(f'Adiconado na posição {indice}')
+    elif cont == 1:                     
+        if valor > lista[0]:
+            lista.insert(1, valor)
+            indice = lista.index(valor)
+            print(f'O valor {valor}, foi adiconado no final da lista!')
         else:
             lista.insert(0, valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
-        # print('O tamanho da lista é ', len(lista))
     elif cont == 2:
-        if lista[0] < valor < lista[1]:
-            lista.insert(1, valor)
+        if valor > lista[1]:
+            lista.insert(2,valor)
             indice = lista.index(valor)
-            print(f'Adicionado na posição {indice}')
-        elif valor> lista[1]:
-            lista.insert(1,valor)
+            print(f'O valor {valor}, foi adiconado no final da lista!')
+        elif valor < lista[0]:
+            lista.insert(0, valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
         else:
-            lista.insert(0,valor)
+            lista.insert(1,valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
     elif cont == 3:
         if valor > lista[2]:
-            lista.insert(2, valor)
+            lista.insert(3, valor)
             indice = lista.index(valor)
-            print(f'Adicionado na posição {indice}')
+            print(f'O valor {valor}, foi adiconado no final da lista!')
         elif valor < lista[0]:
-            lista.insert(lista[0], valor)
+            lista.insert(0, valor)
             indice = lista.index(valor)
-            print(lista.index(valor))
-        elif lista[1] < valor < lista[2]:
+            print(f'Adicionado na posição {indice}')
+        elif valor > lista[1]:
             lista.insert(2, valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
-    else:
+        else:
+            lista.insert(1,valor)
+            indice = lista.index(valor)
+            print(f'Adicionado na posição {indice}')
+    elif cont == 4:
         if valor > lista[3]:
+            lista.insert(4, valor)
+            indice = lista.index(valor)
+            print(f'O valor {valor}, foi adiconado no final da lista!')
+        elif valor < lista[0]:
+            lista.insert(0, valor)
+            indice = lista.index(valor)
+            print(f'Adicionado na posição {indice}')
+        elif valor > lista[2]:
             lista.insert(3, valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
-        elif valor < lista[0]:
-            lista.insert(lista[0], valor)
-            indice = lista.index(valor)
-            print(f'Adicionado na posição {indice}')
-        elif lista[2] < valor < lista[3]:
+        elif valor > lista[1]:
             lista.insert(2, valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
-        else:                                           
-            lista.insert(3, valor)
+        else:
+            lista.insert(1,valor)
             indice = lista.index(valor)
             print(f'Adicionado na posição {indice}')
-print(linha)
+print(f'\n{linha}')
 print(lista)
--- Exercícios/tempCodeRunnerFile.py
+++ Exercícios/tempCodeRunnerFile.py
@@ -1,9 +1,3 @@
-# coding: utf-8
 
-#  No arquivo settings.json no code runner foram alterados:
-
-#        "python": "python -u",
-        # // "python": "set PYTHONIOENCODING=utf-8 && python3.6",
-
-
-#    "code-runner.runInTerminal": false, // true -> para Python e false -> JavqaScript
+            print(lista)
+        else:
