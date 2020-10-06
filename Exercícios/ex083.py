d6ab5ec6cba97c0da96b0a2c6f77073e7da74020
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-06 16:01:07 -0300

    Update ex083.py

--- Exercícios/ex083.py
+++ Exercícios/ex083.py
@@ -5,11 +5,11 @@
 # passada está com os parênteses abertos e fechados na ordem correta. #
 # ******************************************************************* #
 linha = '++'*25
-titulo = ' \033[1;7;31mValidando expressões matemáticas\033[m '
-print(f'\n{titulo:*^60}\n')
+título = ' \033[1;7;31mValidando expressões matemáticas\033[m '
+print(f'\n{título:*^60}\n')
 print(linha)
 # ******************************************************************* #
-expressão = str(input('Disgite uma expressão com parenteses: '))
+expressão = str(input('Digite uma expressão com parênteses: '))
 abre = expressão.count('(')
 fecha = expressão.count(')')
 if abre != fecha:
