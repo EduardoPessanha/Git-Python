5dba861a0c0599115fd826d850e9c04d6e9e1f97
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-07 12:39:44 -0300

    Atualização do arquivo

--- Exercícios/ex078.py
+++ Exercícios/ex078.py
@@ -5,7 +5,7 @@
 # e as suas respectivas posições na lista.                          #
 # ***************************************************************** #
 titulo = ' \033[1;32mMaior e Menor valores na Lista\033[m '
-linha = '*'*60
+linha = '*' * 60
 print(f'\n{titulo:#^70}\n')
 print(f'{linha}\n')
 # ***************************************************************** #
@@ -15,14 +15,14 @@ for valor in range(0, 5):
 print(f'\n{linha}')
 print('Os números digitados foram: ', end='')
 for n in range(0, len(valores)):
-    if n < len(valores)-1:
+    if n < len(valores) - 1:
         print(f'{valores[n]}', end=' ')
     else:
         print(f'{valores[n]}')
 maior = max(valores)
 posM = valores.index(maior) + 1
 menor = min(valores)
-posm = valores.index(menor)+1
+posm = valores.index(menor) + 1
 print(f"O maior valor digitado foi {maior}, nas posições ", end='')
 for pos, c in enumerate(valores):
     if c == maior:
@@ -32,3 +32,32 @@ for pos, c in enumerate(valores):
     if c == menor:
         print(f'{pos + 1}', end=' ')
 print(f'\n{linha}\n')
+
+# ***************************************************************** #
+#                 Solução do Gustavo Guanabara                      #
+# ***************************************************************** #
+# listanun = []
+# mai = 0
+# men = 0
+# for c in range(0, 5):
+#     listanun.append(int(input(f'Digite um valor para a posição {c}: ')))
+#     if c == 0:
+#         mai = men = listanun[c]
+#     else:
+#         if listanun[c] > mai:
+#             mai = listanun[c]
+#         if listanun[c] < men:
+#             men = listanun[c]
+# print(linha)
+# print(f'Você digitou os valores: {listanun}')
+# print(f'O maior valor digitado foi {mai} nas posições: ', end='')
+# for i, v in enumerate(listanun):
+#     if v == mai:
+#         print(f'{i}...', end='')
+# print()
+# print(f'O menor valor digitado foi {men} nas posições: ', end='')
+# for i, v in enumerate(listanun):
+#     if v == men:
+#         print(f'{i}...', end='')
+# print()
+# print(linha)
