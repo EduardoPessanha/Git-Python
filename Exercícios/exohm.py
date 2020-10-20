3ea5921307892f84b7845c553520df634fd00aaf
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-20 10:54:14 -0300

    Atualização do arquivo

--- Exercícios/exohm.py
+++ Exercícios/exohm.py
@@ -1,11 +1,12 @@
 print('\nLei de Ohm -> V = I x R\n')
 fim = ' '
+linha = '-=' * 21
 while fim not in 'N':
     print('''Qual a unidade você quer calcular:
 
         V = Tensão
         I = Corrente
-        R = Resistencia
+        R = Resistência
         ''')
     while True:
         opcao = str(input('Escolha a sua opção (V/I/R): ')).upper().strip()[0]
@@ -14,20 +15,26 @@ while fim not in 'N':
         else:
             break
     if opcao == 'V':
+        print(f'\n{linha}\n')
         I = float(input('Informe o valor da corrente em Aperes: '))
-        R = float(input('Informe o valor da resistencia em Ohms: '))
-        V = I*R
+        R = float(input('Informe o valor da resistência em Ohms: '))
+        V = I * R
         print(f'O valor da tensão é {V:.2f} Volts\n')
+        print(f'{linha}\n')
     elif opcao == 'I':
+        print(f'\n{linha}\n')
         V = float(input('Informe o valor da tensão em Volts: '))
-        R = float(input('Informe o valor da resistencia em Ohms: '))
-        I = V/R
+        R = float(input('Informe o valor da resistência em Ohms: '))
+        I = V / R
         print(f'O valor da corrente é {I:.2f} Aperes\n')
+        print(f'{linha}\n')
     elif opcao == 'R':
+        print(f'\n{linha}\n')
         V = float(input('Informe o valor da tensão em Volts: '))
         I = float(input('Informe o valor da corrente em Aperes: '))
-        R = V/I
-        print(f'O valor da resistencia é {R:.2f} Ohms\n')
+        R = V / I
+        print(f'O valor da resistência é {R:.2f} Ohms\n')
+        print(f'{linha}\n')
     while True:
         fim = str(input('Deseja continuar (S/N): ')).upper().strip()[0]
         if fim not in 'SN':
@@ -38,4 +45,4 @@ while fim not in 'N':
         print()
         break
     else:
-        print('-='*30, '\n')
+        print(f'{linha}\n')
