0c4a27fa2337fb431be3324c0fbe4fe84d34b8ee
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-28 20:31:51 -0300

    Atualização de arquivos

--- Exercícios/ex084.py
+++ Exercícios/ex084.py
@@ -7,7 +7,67 @@
 # C) Uma listagem com as pessoas mais leves.                          #
 # ******************************************************************* #
 linha = '++' * 25
+linha1 = '\033[1;34m*\033[m' * 50
 título = ' \033[1;31mLista composta e análise de dados\033[m '
 print(f'\n{título:*^60}\n')
 print(linha)
 # ******************************************************************* #
+cad = list()
+dados = list()
+# nomeMaiorPeso = list()
+# nomeMenorPeso = list()
+resp = ' '
+maiorPeso = menorPeso = cont = 0
+
+# Criando o cadastro:
+while resp not in 'Nn':
+    dados.append(str(input('Qual é o seu nome: ')))
+    dados.append(float(input('Qual é o seu peso? ')))
+    # Cadastrando o maior e menor peso:
+    if len(cad) == 0:
+        maiorPeso = menorPeso = dados[1]
+    elif dados[1] > maiorPeso:
+        maiorPeso = dados[1]
+    elif dados[1] < menorPeso:
+        menorPeso = dados[1]
+    cad.append(dados[:])
+    dados.clear()
+    while True:
+        resp = str(input('Deseja continuar (S/N)? ')).upper().strip()[0]
+        if resp not in 'SsNn':
+            print('Entrada inválida!', end=' ')
+        else:
+            break
+
+# Verificando o maior e menor peso:
+# for p in cad:
+#     # print(p)
+#     if cont == 0:
+#         maiorPeso = menorPeso = p[1]
+#         cont += 1
+#     elif p[1] > maiorPeso:
+#         maiorPeso = p[1]
+#     elif p[1] < menorPeso:
+#         menorPeso = p[1]
+
+# for i in cad:
+#     if maiorPeso == i[1]:
+#         nomeMaiorPeso.append(i[0])
+#     elif menorPeso == i[1]:
+#         nomeMenorPeso.append(i[0])
+
+print(linha1, '\n')
+print(f'Ao todo, foram cadastradas {len(cad)} pessoas')
+print(f'O maior peso cadastrado foi {maiorPeso:.2f} Kg. Peso de ', end='')
+for p in cad:
+    if maiorPeso == p[1]:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso cadastrado foi {menorPeso:.2f} Kg. Peso de ', end='')
+for p in cad:
+    if menorPeso == p[1]:
+        print(f'[{p[0]}] ', end='')
+print()
+# Na solução do Gustavo Guanabara, ele já fez a verificação do maior e menor Peso
+# no momento do cadastro, antes de adicionar na lista principal,
+# o que reduziu o tamanho do programa.
--- Exercícios/ex085.py
+++ Exercícios/ex085.py
@@ -1,12 +1,25 @@
-# ************************** Desafio 085 **************************** #
-#                   Listas com pares e ímpares                        #
-# Crie um programa onde o usuário possa digitar sete valores numéricos#
-# e cadastre-os em uma lista única que mantenha separados os valores  #
-# pares e ímpares. No final, mostre os valores pares e ímpares em     #
-# ordem crescente.                                                    #
-# ******************************************************************* #
+# ************************** Desafio 085 ***************************** #
+#                   Listas com pares e ímpares                         #
+# Crie um programa onde o usuário possa digitar sete valores numéricos #
+# e cadastre-os em uma lista única que mantenha separados os valores   #
+# pares e ímpares. No final, mostre os valores pares e ímpares em      #
+# ordem crescente.                                                     #
+# ******************************************************************** #
 linha = '++' * 25
+linha1 = '\033[1;34m*=\033[m'*25
 título = ' \033[1;32mListas com pares e ímpares\033[m '
 print(f'\n{título:*^60}\n')
 print(linha)
 # ******************************************************************* #
+geral = [[], []]
+for n in range(0, 7):
+    num = int(input(f'Digite o {n + 1}º valor: '))  # Lendo os 7 números:
+    if num % 2 != 0:    # Número digitado é ímpar
+        geral[1].append(num)
+    else:   # Número digitado é par
+        geral[0].append(num)
+geral[1].sort()
+geral[0].sort()
+print(linha1)
+print(f'Os valores pares digitados foram: {geral[0]}')
+print(f'Os valores ímpares digitados foram: {geral[1]}')
