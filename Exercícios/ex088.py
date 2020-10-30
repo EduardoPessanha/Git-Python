cc9d6132802fec781b94fe306c806a7e6dbe5bf0
Eduardo Pessanha <poppessanha@gmail.com>
2020-10-30 16:24:53 -0300

    Atualização de arquivo

--- Exercícios/ex088.py
+++ Exercícios/ex088.py
@@ -5,8 +5,25 @@
 # números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista   #
 # composta.                                                            #
 # ******************************************************************** #
+from random import randint
 linha = '++' * 25
+linha1 = '\033[1;34m*=\033[m' * 25
 título = ' \033[1;35mPalpites para a Mega Sena\033[m '
 print(f'\n{título:*^60}\n')
 print(linha)
 # ******************************************************************* #
+palpite = list()
+mega = [[], [], [], [], [], []]
+quant = 10
+for jogo in range(0, quant):
+    for lin in range(0, 6):
+        # for col in range(0, 6):
+        mega[lin] = randint(1, 60)
+    mega.sort()
+    palpite.append(mega[:])
+print(linha1)
+for lin in range(0, quant):
+    print(f'Jogo {lin + 1}: {palpite[lin]}')
+print(linha1)
+
+# video: 32:56
