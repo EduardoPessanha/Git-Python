290e1de9777b8859aab936a7ccbe11efa077ff72
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-02 12:43:34 -0300

    Atualização - Versão 02

--- Exercícios/exmatriz.py
+++ Exercícios/exmatriz.py
@@ -1,3 +1,9 @@
+linha = '++' * 25
+linha1 = '\033[1;34m*=\033[m' * 21
+título = ' \033[1;33mMatriz em Python\033[m '
+print(f'\n{título:*^60}\n')
+print(linha)
+# # ******************************************************************* #
 matriz = list()
 '''
 lin = nº de linhas
@@ -7,7 +13,7 @@ valor = valor do elemento da matriz
 print()
 lin = int(input('Digite o número de linhas da matriz: '))
 col = int(input('Digite o número de colunas da matriz: '))
-print()
+print(linha1)
 for l in range(0, lin):
     # Cria as linhas da matriz
     linha = []
@@ -15,8 +21,9 @@ for l in range(0, lin):
         # Cria as colunas da matriz e recebe o valor
         linha.append(int(input(f'Digite o valor para a posição [{l}][{c}]: ')))
     matriz.append(linha)
-for l, v in enumerate(matriz):
+print(linha1)
+for l in range(lin):
+    for c in range(col):
+        print(f'[{matriz[l][c]:^6}]', end=' ')
     print()
-    for l1, vi in enumerate(v):
-        print(f'[{vi:^6}]', end='')
-print()
+print(linha1)
