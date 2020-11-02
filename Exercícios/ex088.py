1c7b00d98eb087644984ceecfa362e3d3c519c40
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-01 21:06:38 -0300

    Atualização de arquivo

--- Exercícios/ex088.py
+++ Exercícios/ex088.py
@@ -7,7 +7,6 @@
 # ******************************************************************** #
 from random import randint
 from time import sleep
-
 linha = '++' * 25
 linha1 = '\033[1;34m*=\033[m' * 25
 título = ' Palpites para a Mega Sena '
@@ -37,4 +36,31 @@ print('\033[1;34m*=' * 7, f' SORTEANDO {quant} JOGOS ', '*=' * 7, '\033[m')
 for lin in range(0, quant):
     print(f'Jogo {lin + 1}: {palpite[lin]}')
     sleep(1)
-print('\033[1;34m*=' * 9, ' BOA SORTE!!! ', '*=' * 9, '\033[m')
+print('\033[1;34m*=' * 9, ' BOA SORTE!!', '*=' * 9, '\033[m')
+
+# **************************** SOLUÇÃO DO GUSTAVO GUANABARA ****************************
+# jogo = list()
+# quant = int(input('Quantos jogos você quer sortear? '))
+# print('\033[1;34m*=' * 7, f' SORTEANDO {quant} JOGOS ', '*=' * 7, '\033[m')
+# tot = 1
+# palpite = list()
+# while tot <= quant:
+#     cont = 0
+#     # ****** Sorteando os números:
+#     while True:
+#         num = randint(1, 60)
+#         if num not in jogo:
+#             jogo.append(num)
+#             cont += 1
+#         if cont >= 6:
+#             break
+#     # ****************************
+#     tot += 1
+#     jogo.sort()
+#     palpite.append(jogo[:])
+#     jogo.clear()
+# for i, j in enumerate(palpite):
+#     print(f'Jogo n.º {i + 1}: {j}')
+#     sleep(1)
+# # print('.*' * 19)
+# print('\033[1;34m*=' * 9, ' BOA SORTE!!', '*=' * 9, '\033[m')
