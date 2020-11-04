160348a9cb4a57606908cb536a28ed32a79a9a5d
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-04 12:44:03 -0300

    Atualização de arquivo

--- Exercícios/ex092.py
+++ Exercícios/ex092.py
@@ -4,14 +4,32 @@
 # trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a    #
 # CTPS for diferente de ZERO, o dicionário receberá também o ano de     #
 # contratação e o salário. Calcule e acrescente, além da idade, com     #
-# quantos anos a pessoa vai se aposentar..                              #
+# quantos anos a pessoa vai se aposentar.                               #
 # ********************************************************************* #
-from random import randint
-from time import sleep
-from operator import itemgetter
+from datetime import datetime
 linha = '++' * 24
 linha1 = '\033[1;34m*=\033[m' * 24
-título = ' \033[1;3;4;7;33mCadastro de Trabalhador em Python\033[m '
+título = ' \033[1;3;4;7;34mCadastro de Trabalhador em Python\033[m '
 print(f'\n{título:*^64}\n')
 print(linha)
 # ********************************************************************* #
+# cad = {'nome': '', 'idade': '', 'CTPS': ''}
+# print(cad)
+cad = dict()
+cad['nome'] = str(input('Nome: ')).capitalize().strip()
+cad['idade'] = datetime.now().year - (int(input('Ano de Nascimento: ')))
+cad['CTPS'] = int(input('Carteira de Trabalho - CTPS (0 não tem): '))
+if cad['CTPS'] != 0:
+    cad['contratação'] = int(input('Ano de contratação: '))
+    cad['salário'] = float(input('Salário: R$ '))
+    # Para o exercício consideraremos a aposentadoria após 35 anos de trabalho
+    cad['aposentadoria'] = cad['contratação'] + 35 - datetime.now().year + cad['idade']
+    print(linha1)
+    for k, v in cad.items():
+        print(f'{"- ":>6}{k.capitalize()} tem o valor {v}')
+    print(linha1)
+else:
+    print(linha1)
+    for k, v in cad.items():
+        print(f'{"- ":>6} {k.capitalize()} tem o valor {v}')
+    print(linha1)
