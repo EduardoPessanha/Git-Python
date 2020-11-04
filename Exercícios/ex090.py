36df6fdb4462076836e6f11b2fadcc1e229aab88
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-03 22:35:36 -0300

    Revisão e atualização

--- Exercícios/ex090.py
+++ Exercícios/ex090.py
@@ -5,7 +5,23 @@
 # estrutura na tela.                                                  #
 # ******************************************************************* #
 linha = '++' * 24
-título = ' \033[1:3:4:7:37mDicionário em Python\033[m '
+título = ' \033[1;3;4;7;37mDicionário em Python\033[m '
 print(f'\n{título:*^64}\n')
 print(linha)
 # ******************************************************************* #
+boletim = dict()
+boletim['aluno'] = str(input('Nome do aluno: ')).capitalize()
+boletim['média'] = float(input(f'Média de {boletim["aluno"]}: '))
+if boletim['média'] < 5 :
+    boletim['situação'] = 'REPROVADO'
+elif boletim['média'] < 7:
+    boletim['situação'] = 'RECUPERAÇÃO'
+else:
+    boletim['situação'] = 'APROVADO'
+print(linha)
+# print(f'O nome do aluno é {boletim["aluno"]}')
+# print(f'A média é igual a {boletim["media"]}')
+# print(f'Situação é igual a {boletim["situação"]}')
+for k, v in boletim.items():
+    print(f'- {k.capitalize()} é igual a {v}')
+print(linha)
