9efe9e8d79092c4c42c21564bbb392988b116ec1
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-05 17:41:13 -0300

    Atualização

--- Exercícios/ex095.py
+++ Exercícios/ex095.py
@@ -5,14 +5,14 @@
 # de cada jogador.                                                   #
 # ****************************************************************** #
 linha = '+=' * 24
-linha1 = '\033[1;34m*=\033[m' * 24
+linha1 = '\033[1;34m*=\033[m' * 26
 título = ' \033[1;3;4;7;34mAprimorando os Dicionários\033[m '
 print(f'\n{título:*^64}\n')
 print(linha)
 # ****************************************************************** #
 cad = dict()
 n_gols = list()
-lista = list()
+jogos = list()
 # Cadastrando os jogadores:
 while True:
     cad.clear()
@@ -28,7 +28,7 @@ while True:
         if resp in 'SN':
             break
         print('Entrada INVÁLIDA. ', end='')
-    lista.append(cad.copy())
+    jogos.append(cad.copy())
     if resp == 'N':
         break
 # Fim do cadastro
@@ -36,8 +36,44 @@ print(linha1)
 #  Exibindo a listagem geral dos jogadores:
 print(f"{'Cod':^6}{'Nome':<12}{'Gols':<18}{'Total':^6}")
 print(linha)
-for k, v in enumerate(lista):
+for k, v in enumerate(jogos):
     print(f"{(k + 1):^6}{v['nome']:<12}{str(v['gols']):<18}{v['total']:}")
 print(linha1)
-# Exibindo a listagem por jogador:
+# Exibindo os dados do jogador:
+while True:
+    cod = int(input('Mostrar dados de qual jogador? (999 para sair) '))
+    if cod == 999:
+        break
+    elif cod <= partidas:
+        # O indice da lista jogos corresponde a um dicionário que contém os dados dos jogadores:
+        i = cod - 1
+        print(f"-- LEVANTAMENTO DO JOGADOR {jogos[i]['nome']} --")
+        # O números de partidas corresponde aos gols que estão na chave 'gols' do dicionário que contém os
+        # dados do jogador print(jogos[i]['gols'])
+        for k in range(len(jogos[i]['gols'])):
+            print(f"Na partida {k + 1} fez ", end='')
+            print(f"{jogos[i]['gols'][k]} gols.")
+        print(linha1)
+    else:
+        print(f'NÃO EXISTE jogador com código {cod}. Tente outra vez!!!')
+print(linha1)
+print(f'{"< << << << PROGRAMA ENCERRADO >> >> >> >":^52}')
 
+'''
+ordenando raciocínio:
+jogos é a lista que contém os dicionários (cad) com os dados dos jogadores.
+
+jogos = [{'nome': 'A', 'gols': [1, 2, 3, 4], 'total': 10}, {'nome': 'B', 'gols': [1, 2, 3], 'total': 6},
+         {'nome': 'C', 'gols': [1, 2], 'total': 3}]
+
+Então cada indice de jogos corresponde as dados de um jogador:
+
+jogos[0] = {'nome': 'A', 'gols': [1, 2, 3, 4], 'total': 10}
+
+logo o nome do jogador x  será o valor do campo 'nome' de jogos[x]
+
+
+#     item                item            item
+#  ___________   ___________________   __________
+# {'nome': 'T', 'gols': [2, 1, 2, 3], 'total': 8}
+'''
