cf9f4958ce8bbfcba79be3dbe4fbf7db1c49a92b
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-08 14:20:09 -0300

    Criação do arquivo ex096.py

---
+++ Exercícios/ex096.py
@@ -0,0 +1,12 @@
+# **************************** Desafio 096 ************************* #
+#                       Função que calcula área                      #
+# Faça um programa que tenha uma função chamada área(), que receba   #
+# as dimensões de um terreno retangular (largura e comprimento) e    #
+# mostre a área do terreno.                                          #
+# ****************************************************************** #
+lin = '+=' * 20
+lin1 = '\033[1;34m*=\033[m' * 26
+título = ' \033[1;3;4;7;34mFunção que calcula área\033[m '
+print(f'\n{título:*^64}\n')
+print(lin)
+# ****************************************************************** #
