23cc987c795ae0812e2dc9d2979fd3925d3ddae7
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-08 16:22:50 -0300

    Atualização do arquivo ex096.py

--- Exercícios/ex096.py
+++ Exercícios/ex096.py
@@ -4,9 +4,33 @@
 # as dimensões de um terreno retangular (largura e comprimento) e    #
 # mostre a área do terreno.                                          #
 # ****************************************************************** #
-lin = '+=' * 20
-lin1 = '\033[1;34m*=\033[m' * 26
-título = ' \033[1;3;4;7;34mFunção que calcula área\033[m '
-print(f'\n{título:*^64}\n')
-print(lin)
+#
+# Definindo as funções:
+def lin():
+    print('+=' * 24)
+
+
+def lin1():
+    print('\033[1;34m*=\033[m' * 26)
+
+
+def titulo(msg):
+    lin()
+    texto = f' \033[1;3;4;7;34m{msg}\033[m '
+    print(f'\n{texto:*^64}\n')
+    lin()
+    print()
+
+
+def area(larg, comp): # Calcula a área
+    s = larg * comp
+    print(f'A área do terreno é igual a {s:.2f}m²')
+
+
 # ****************************************************************** #
+# Rotina principal
+titulo('Função que calcula área')
+# larg = float(input("Qual a largura do terreno? "))
+# comp = float(input('Qual o comprimento do terreno? '))
+area(float(input("Qual a largura do terreno? ")), float(input('Qual o comprimento do terreno? ')))
+lin1()
