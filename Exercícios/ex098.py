5a660e5635b7d890f239226c16d604b40fa68ff9
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-09 15:36:45 -0300

    Atualização do arquivo ex098.py versão 01

--- Exercícios/ex098.py
+++ Exercícios/ex098.py
@@ -27,5 +27,32 @@ def titulo(msg):
 
 
 titulo('Função de Contador')
-# ****************************************************************** #
 
+
+# ****************************************************************** #
+# def exibe(início, fim, passo):
+#     print(f'Contagem de {início} até {fim} de {passo} em {passo}:')
+
+
+def contador(ini, fim, p):
+    if p < 0:
+        p = -p
+    if p == 0:
+        p = 1
+    print(f'Contagem de {ini} até {fim} de {p} em {p}:')
+    if ini < fim:
+        for c in range(ini, fim + 1, p):
+            print(c, end=' ')
+    else:
+        for c in range(ini, fim - 1, -p):
+            print(c, end=' ')
+    print('FIM!.')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('Agora é a sua vez de personalizar a contagem!')
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contador(i, f, p)
