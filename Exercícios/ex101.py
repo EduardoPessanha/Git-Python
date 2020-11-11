6cb47029716fdda89c3e2dfacc62f564f23f9135
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-11 14:56:11 -0300

    Atualização de arquivo

--- Exercícios/ex101.py
+++ Exercícios/ex101.py
@@ -9,28 +9,46 @@ from utilitarios import *
 #
 titulo(' Funções para votação ')
 # ****************************************************************** #
-# Definindo as funções:
 
 
+# Definindo a função:
 def voto(ano):
     """
-    Recebe o ano de nascimento de uma pessoa
-    e analisa a condição de voto de acordo
-    com a idade.
+    -> Recebe o ano de nascimento de uma pessoa
+    e analisa a condição de voto de acordo com
+    a idade.
     :param ano: ano de nascimento
-    :teste: somente um teste
     :return: idade e status
+    Criado por EAMP
     """
     from datetime import datetime
     idade = datetime.now().year - ano
     if idade < 16:
         status = 'NÃO VOTA'
-    elif 16 <= idade < 18 or idade >= 65:
+    elif 16 <= idade < 18 or idade > 65:
         status = 'VOTO OPCIONAL'
     else:
         status = 'VOTO OBRIGATÓRIO'
     return idade, status
 
 
+# Rotina principal:
 a = voto(int(input('Em que ano você nasceu? ')))
-print(f'Com {a[0]} anos: {a[1]}')
+print(f'Com {a[0]} anos: {a[1]}.')
+
+# # Solução do Gustavo Guanabara:
+#
+# def voto(ano):
+#     from datetime import date
+#     atual = date.today().year
+#     idade = atual - ano
+#     if idade < 16:
+#         return print(f'Com {idade} anos: NÃO VOTA.')
+#     elif 16 <= idade < 18 or idade > 65:
+#         return print(f'Com {idade} anos: VOTO OPCIONAL.')
+#     else:
+#         return print(f'Com {idade} anos: VOTO OBRIGATÓRIO.')
+#
+#
+# nasc = int(input('Em que ano você nasceu? '))
+# voto(nasc)
