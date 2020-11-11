7ba1616fe9cded5938e854c866ebacfae3daf514
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-10 21:51:13 -0300

    Update tempCodeRunnerFile.py

--- Aulas/tempCodeRunnerFile.py
+++ Aulas/tempCodeRunnerFile.py
@@ -1,7 +1,19 @@
-valor = list()
-for cont in range(0, 5):
-    # inserindo os elementos da lista pelo teclado!
-    valor.append(int(input('Digite um valor: ')))
-for c, v in enumerate(valor):
-    print(f'Na posição {c} encontrei o valor {v}!')
-print('Cheguei ao final da lista')
+
+    #     tipo = '\033[m'
+    # elif tipo == 'br':
+    #     tipo = '\033[1;30m'
+    # elif tipo == 'vm':
+    #     tipo = '\033[1;31m'
+    # elif tipo == 'vd':
+    #     tipo = '\033[1;32m'
+    # elif tipo == 'am':
+    #     tipo = '\033[1;33m'
+    # elif tipo == 'az':
+    #     tipo = '\033[1;34m'
+    # elif tipo == 'mg':
+    #     tipo = '\033[1;35m'
+    # elif tipo == 'ci':
+    #     tipo = '\033[1;36m'
+    # elif tipo == 'cz':
+    #     tipo = '\033[1;37m'
+    # return tipo
