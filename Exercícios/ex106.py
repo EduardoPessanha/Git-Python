cc8bd1d9c884e695333e53f7c8d145d0f975162b
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-13 09:01:02 -0300

    AAtualização de arquivo

--- Exercícios/ex106.py
+++ Exercícios/ex106.py
@@ -12,27 +12,40 @@ titulo(' Sistema interativo de ajuda em Python ')
 
 
 # Definindo as funções:
-def pyhelp(nome):
+def pyhelp():
+    while True:
+        texto = "SISTEMA DE AJUDA PyHELP:"
+        t = len(texto) + 4
+        print(f'\033[m\033[1;30;42m*' * t)
+        print(f'{texto:^{t}}')
+        print(f"{'*' * t}")
+        func = (str(input('\033[mFunção de Biblioteca -> ')))
+        if func == 'fim':
+            break
+        else:
+            manual(func)
+    saida()
+
+
+def manual(nome):
     from time import sleep
     texto = f"Acessando o manual do comando '{nome}'"
-    print(f'\033[1;30;44m*' * (len(nome) + 37))
-    print(f'{texto:>38}')
-    print('*' * (len(nome) + 37))
+    t = len(texto) + 4
+    print(f'\033[1;30;44m*' * t)
+    print(f'{texto:^{t}}')
+    print('*' * t)
     print('\033[m\033[1;7;30m')
     help(nome)
     sleep(2)
 
 
-# Rotina principal
-while True:
-    print(f'\033[m\033[1;30;42m*' * 28)
-    print(f'{"SISTEMA DE AJUDA PyHELP:":^28}')
-    print(f"{'*' * 28}")
-    func = (str(input('\033[mFunção de Biblioteca -> ')))
-    if func == 'fim':
-        break
-    else:
-        pyhelp(func)
-print(f'\033[m\033[1;30;41m*' * 14)
-print(f'{"ATÉ LOGO!:":^14}')
-print(f"{'*' * 14}")
+def saida():
+    texto = '"ATÉ LOGO!:"'
+    t = len(texto) + 4
+    print(f'\033[m\033[1;30;41m*' * t)
+    print(f'{texto:^{t}}')
+    print(f"{'*' * t}")
+
+
+# Rotina principal:
+pyhelp()
